cea64966f5c4617a2992854ed61d2a9c70e0fe36
Gonzalo García <snorkellingcactus@gmail.com>
2014-10-10 07:05:29 -0300

    -Se agregó log.js -Se pasó todo a POO.
    -Objetos:
    	Interp:
    		Se encarga (de momento) de interpretar texto para pasarlo a un objeto Exp.
    		Hasta ahora admite polinomios del tipo ax^e+by^f y así...
    		Puede admitir paréntesis con multiplicaciones para los exponentes.
    
    		Se le puede pasar un texto como parámetro en la creación para que lo interprete.
    		La expresión resultante queda en Interp.expresion.

--- index.js
+++ index.js
@@ -1,50 +1,362 @@
-//Salida:
-log=new Log();
+Interp=function()
+{
+	//Flags de operaciones en tiempo de interpretación.
+	this.expo=false;
+	this.mult=false;
+	this.res=false;
+	this.fn=false;
+
+	//Entorno.
+	this.pos=0;
+	this.signo=1;
+	this.incog=0;
+	this.num=0;
+	this.buff='';
+	this.str='';
+	this.monomio={};
+
+	//Objeto de logeo para salidas verbosas.
+	this.log=new Log();
+
+	if(arguments.length)
+	{
+		this.interpStr(arguments[0]);
+	}
+}
+Exp=function()
+{
+	//Distintos arrays que permiten referenciar monomios
+	//según distintos patrones.
+	this.refs={'incogs':['']};
+
+	//Lista de monomios.
+	this.monomios=[];
+
+	if(arguments.length)
+	{
+		this.interpStr(arguments[0]);
+	}
+}
+Exp.prototype.interpStr=function(str)
+{
+	var interp=new Interp(this);
+
+	return interp.interpStr(str);
+}
+Exp.prototype.insMonomio=function(monomio)
+{
+	var incogStr='';
+	if(monomio.incogs.length)
+	{
+		//Ordeno incognitas por orden alfabético.
+		monomio.incogs=monomio.incogs.sort();
+
+		//Formateo un string con las incognitas seguidas por su exponente.
+		for(var i=0;i<monomio.incogs.length;i++)
+		{
+			var incogAct=monomio.incogs[i];
+			incogStr+=incogAct;
+			incogStr+=monomio[incogAct][1];
+		}
+	}
+	//Si no existía esa clave en el array la creo.
+	if(!this.refs.incogs[incogStr])
+	{
+		this.refs.incogs[incogStr]=[];
+	}
+	//Si agrego el ID del monomio a la lista de referencias por incógnitas.
+	this.refs.incogs[incogStr].push(this.monomios.length);
+	//Agrego el monomio a la expresión.
+	this.monomios.push(monomio);
+}
+//Función que borra espacios y puntos de un string
+//que va a ser interpretado.
+Interp.prototype.clrStr=function(str)
+{
+	this.log.txt("Aplicando RegExp");
+	this.str=this.str.replace( /\s|\./g , "");
+}
+Interp.prototype.letraDef=function(letra)
+{
+	if(this.incog)
+	{
+		if(!this.expo)
+		{
+			log.txt("Buffer = "+this.buff);
+			this.buff=parseFloat(this.buff);
+			if(!this.buff && this.buff!==0)
+			{
+				this.buff=1;
+			}
+			log.txt("Buffer = "+this.buff);
+			
+			this.log.txt("Se determino 1 al exponente de "+this.incog);
+			this.monomio[this.incog][1]=1;
+			this.monomio[this.incog][0]=(this.monomio[this.incog][0]||1)*(this.buff)*this.signo;
+
+			this.buff="";
+			this.signo=1;
+
+			this.log.txt("Se determinó "+this.monomio[this.incog][0]+" para el coheficiente de "+this.incog)
+		}
+		else
+		{
+			
+		}
+	}
+	if(!this.monomio[letra])
+	{
+		//Nueva incógnita.
+		this.log.txt("Se trata de una nueva incógnita");
+
+		this.mkMult();
+
+		this.monomio[letra]=[];
+		this.monomio[letra][0]=(parseFloat(this.buff)||this.num)*this.signo;
+		this.monomio[letra][1]=1;
+		if(!this.monomio.incogs)
+		{
+			this.monomio.incogs=[];
+		}
+		this.monomio.incogs.push(letra);
+
+		this.buff="";
+		this.signo=1;
+		this.log.txt("creada nueva incógnita "+letra);
+
+		log.array();
+		log.array(this.monomio[letra],letra);
+		log.array();
+	}
+	this.incog=letra;
+}
+//Deriva a la función indicada dependiendo de la letra que se encontró.
+Interp.prototype.letraOp=function(letra)
+{
+	this.log.txt("Se encontró una letra. ( "+letra+" )");
+	switch(letra)
+	{
+		case '^':
+			this.opExpo(letra);
+		break;
+		case '*':
+			this.opMult(letra);
+		break;
+		case ',':
+			this.opComa(letra);
+		break;
+		case '(':
+			this.opParIni(letra);
+		break;
+		case ')':
+			this.opParFin(letra);
+		break;
+		case '-':
+			this.opMenos(letra);
+		break;
+		case '+':
+			this.opMas(letra);
+		break;
+		case '':
+
+		break;
+		default:
+		this.letraDef(letra);
+	}
+}
+Interp.prototype.numOp=function(num)
+{
+	this.log.txt("Se encontró un numero. ( "+num+" )");
+	this.buff+=num;
+
+	if(this.expo&&this.mult)
+	{
+		//this.log.txt("Error, exponenciando y multiplicando a la vez");
+	}
+}
+Interp.prototype.interpStr=function(str)
+{
+	if(!this.expresion)
+	{
+		this.expresion=new Exp();
+	}
+	this.log.txt("Convirtiendo monomios en arrays para coleccionarlos:");
+
+	this.str=str;
+	//Limpio el texto.
+	this.clrStr(str);
+
+	this.log.txt("Entrada: "+this.str);
+
+	//El nuevo monomio.
+	monomio={};
+
+	this.pos=0;
+
+	while(this.pos<this.str.length)
+	{
+		//Decido que se va a hacer con el caracter dependiendo de
+		//Si es letra o número.
+		if(isNaN(this.str[this.pos]))
+		{
+			this.letraOp(this.str[this.pos]);
+		}
+		else
+		{
+			this.numOp(this.str[this.pos]);
+		}
+
+		++this.pos;
+	}
+
+	if(this.expo)
+	{
+		this.expo=false;
+		this.monomio[this.incog][1]=parseFloat(this.buff);
+	}
+
+	this.expresion.insMonomio(this.monomio);
+
+	this.log.txt('Lista monomios:');
+	log.array()
+	log.array(this.expresion.monomios);
+	log.array();
+	this.log.txt('Lista referencias según incógnitas:');
+	log.array();
+	log.array(this.expresion.refs.incogs);
+	log.array();
+	log.sep();
+	return this.expresion;
+};
+Interp.prototype.floatBuff=function()
+{
+	this.num=parseFloat
+	(
+		this.buff
+	)||1;
+}
+Interp.prototype.mkMult=function()
+{
+	if(this.mult)
+	{
+		this.num=parseFloat(this.buff)*this.num;
+
+		this.log.txt('Una multiplicación resultó '+this.num);
+		this.buff="";
+		this.mult=false;
+	}
+}
+//Cuando se indica una exponenciación (^);
+Interp.prototype.opExpo=function()
+{
+	this.floatBuff();
+	this.buff="";
+
+	this.expo=true;
+
+	this.log.txt("Se realizará una potenciación");
+}
+//Cuando se indica una multiplicación (^);
+Interp.prototype.opMult=function()
+{
+	this.floatBuff();
+	this.buff=""
+	this.mult=true;
+
+	this.log.txt("Se realizará una multiplicación");
+}
+Interp.prototype.opComa=function()
+{
+	this.buff+=".";
+
+	this.log.txt("Es una coma");
+}
+Interp.prototype.opParIni=function()
+{
+	if(!this.expo)
+	{
+		var posFin=str.substr(this.pos).indexOf(")")+this.pos;
+		var incog=this.incog+str.substr(this.pos,posFin);
+		if(!fns[incog])
+		{
+			incog+=str.substr
+			(
+				this.pos,
+				posFin
+			);
+			this.pos+=posFin;
+		}
+	}
+}
+Interp.prototype.opParFin=function()
+{
+	if(this.expo&&this.mult)
+	{
+		this.expo=false;
+		this.mkMult();
+		this.monomio[this.incog][1]=this.num;
+		this.log.txt('Se (re)determinó '+this.monomio[this.incog][1]+' para el exponente.');
+	}
+}
+Interp.prototype.opMas=function()
+{
+	//Si se estaba procesando un exponente
+	//Lo guardo 
+	if(this.expo&&this.buff.length)
+	{
+		this.expo=false;
+		this.monomio[this.incog][1]=parseFloat(this.buff)*this.signo;
+
+		this.signo=1;
+		this.buff="";
+
+		this.log.txt("Se determinó el exponente "+this.monomio[this.incog][1]+" para la incognita "+this.incog);
+	}
+	if(!(this.expo&&this.mult&&this.res))
+	{
+		this.expresion.insMonomio(this.monomio);
+
+		this.log.txt("Insertado monomio:");
+		log.array();
+		log.array(this.monomio);
+		log.array();
+
+		//Vacío lo que va a ser el nuevo monomio.
+		this.monomio={};
+
+		this.expo=false;
+		this.res=false;
+		this.fn=false;
+		this.incog=0;
+	}
+}
+Interp.prototype.opMenos=function()
+{
+	this.signo=-1;
+	this.opSuma();
+}
 //Formatea un polinomio escrito para que pueda ser procesado por las funciones (borra espacios y puntos).
 function clrStrPol(strPol)
 {
-	log.txt("Aplicando RegExp");
-	return strPol.replace( /\s|\./g , "");
+	return strPol;
 }
 //Convierte una cadena con un termino en un array con coheficiente, incógnitas y exponente [0,1,2].
-function strMonArr(strMon)
+/*function strMonArr(strMon)
 {
-
-	log.txt("Entrada:"+strMon);
-	var incogs={};
-	var modoExpo=false;
-	var modoMult=false;
-	var resuelve=false;
-	var modoFn=false;
-
-	var incogAct=0;
-	var j=0;
-	var numBuff="";
-	var num=0;
-
-	log.txt("Analizando Monomio...");
-	while(j<strMon.length)
+	this.log.txt("Analizando Monomio...");
+	while(j<str.length)
 	{
 		if(isNaN(strMon[j]))
 		{
-			log.txt("Se encontró una letra. ( "+strMon[j]+" )");
 			switch(strMon[j])
 			{
 				case "^":
-					num=parseFloat(numBuff)||1;
-					numBuff="";
-					modoExpo=true;
-				log.txt("Se realizará una potenciación");
+				
 				break;
 				case "*":
-					num=parseFloat(numBuff)||1;
-					numBuff="";
-					modoMult=true;
-				log.txt("Se realizará una multiplicación");
+
 				break;
 				case ",":
-					numBuff+=".";
-				log.txt("Es una coma");
 				break;
 				case "(":
 					if(!modoExpo)
@@ -60,18 +372,30 @@ function strMonArr(strMon)
 				break;
 				case ")":
 				modoExpo=false;
-				incogs[incogAct][1]=parseFloat(numBuff);
+				monomio[incogAct][1]=parseFloat(numBuff);
+				break;
+				case "-":
+				signo=-1;
+				case "+":
+				
 				break;
 				default:
 					if(incogAct)
 					{
 						if(!modoExpo)
 						{
-							log.txt("Se determino 1 al exponente de "+incogAct);
-							incogs[incogAct][1]=1;
-							incogs[incogAct][0]=(incogs[incogAct][0]||1)*(parseFloat(numBuff));
-							log.txt("Se determinó "+incogs[incogAct][0]+" para el coheficiente de "+incogAct)
+							var numBuff=parseFloat(numBuff);
+							if(!numBuff && numBuff!==0)
+							{
+								numBuff=1;
+							}
+							
+							this.log.txt("Se determino 1 al exponente de "+incogAct);
+							monomio[incogAct][1]=1;
+							monomio[incogAct][0]=(monomio[incogAct][0]||1)*(numBuff)*signo;
+							this.log.txt("Se determinó "+monomio[incogAct][0]+" para el coheficiente de "+incogAct)
 							numBuff="";
+							signo=1;
 						}
 						else
 						{
@@ -81,24 +405,31 @@ function strMonArr(strMon)
 					else
 					{
 						//Nueva incógnita.
-						log.txt("Se trata de una nueva incógnita");
+						this.log.txt("Se trata de una nueva incógnita");
 						if(modoMult)
 						{
 							var tmp=num;
-							num=tmp*(parseFloat(numBuff)||1);
-							numBuff="";
+							num=tmp*parseFloat(numBuff);
+							this.log.txt(tmp+" X "+numBuff+" = "+num);
+							numBuff=""+num;
 							modoMult=false;
 						}
 					}
-					if(!incogs[strMon[j]])
+					if(!monomio[strMon[j]])
 					{
-						incogs[strMon[j]]=[];
-						incogs[strMon[j]][0]=(parseFloat(numBuff)||1);
-						incogs[strMon[j]][1]=1;
+						monomio[strMon[j]]=[];
+						monomio[strMon[j]][0]=(parseFloat(numBuff)||num)*signo;
+						monomio[strMon[j]][1]=1;
+						if(!monomio.incogs)
+						{
+							monomio.incogs=[];
+						}
+						monomio.incogs.push(strMon[j]);
 						numBuff="";
-						log.txt("creada nueva incógnita "+strMon[j]);
+						signo=1;
+						this.log.txt("creada nueva incógnita "+strMon[j]);
 						log.array();
-						log.array(incogs[strMon[j]],strMon[j]);
+						log.array(monomio[strMon[j]],strMon[j]);
 						log.array();
 					}
 					incogAct=strMon[j];
@@ -106,50 +437,86 @@ function strMonArr(strMon)
 		}
 		else
 		{
-			log.txt("Se encontró un numero. ( "+strMon[j]+" )");
-			numBuff+=strMon[j];
-			if(modoExpo&&modoMult)
-			{
-				log.txt("Error, exponenciando y multiplicando a la vez");
-			}
+			
 		}
 		j++;
 	}
 	
-	if(modoExpo)
-	{
-		modoExpo=false;
-		incogs[incogAct][1]=parseFloat(numBuff);
-	}
-	log.array()
-	log.array(incogs);
-	log.array();
-	log.sep();
-	return incogs;
+	
 	//return res;
-};
+};*/
 //Convierte una cadena preprocesada por clrStrPol en un array con monomios
+/*
 function strPolArr(strPol)
 {
-	log.txt("Separando monomios:");
+	this.log.txt("Separando monomios:");
 
 	var splPol=strPol.split(/\+|\-/g);
 	var polArr=[];
 
 	return splPol;
-}
+}*/
 //Ordena array de monomios por los exponentes de cada uno.
 function reordenaCohef(polArr)
 {
-	var cohefs=[];
-	var refPolCohef=[];
-	var ordenado=[];
+	var cohefs=[];		//Lista de coheficientes.
+	var refPolIncogs=[];		//Para referenciar a los polinomios según su cantidad de incógnitas.
+	var refPolCohef=[];	//Para referenciar a los polinomios según su exponente.
+	var ordenado=[];	//Resultado ordenado segun exponentes.
 	
+	this.log.txt('Entrada:');
+	log.array();
+	log.array(polArr);
+	log.array();
+	this.log.txt("Creando lista de coheficientes:");
 	for(var i=0;i<polArr.length;i++)
 	{
-		cohefs[i]=polArr[i][2];
-		refPolCohef[polArr[i][2]]=polArr[i];
+		//Agrego el polinomio en refPolIncogs según su cantidad
+		//De incógnitas.
+		nIncogsAct=polArr.incogs.length;
+		if(!refPolIncogs[nIncogsAct])
+		{
+			refPolIncogs[nIncogsAct]=[];
+		}
+		refPolIncogs[nIncogsAct].push(polArr[i]);
 	};
+	return 0;
+	/*
+	for(var clave as refPolIncogs)
+	{
+		var polsIncogAct=refPolIncogs[clave];	//Lista de polinomios actual.
+
+		for(var j=0;j<refPolIncogs[clave].length;j++)
+		{
+			var polAct=polsIncogAct[j];		//Polinomio actual.
+			var grupoIncogs={};				//Lista de nombres de incógnitas con su exponente como valor.
+			var agrupable=1;			//Por defecto es agrupable
+			
+			for(var k=1;k<parseInt(clave);k++)
+			{
+				var incogAct=polAct.incogs[k];		//Obtengo nombre de incognita.
+				var expoAct=polAct[incogAct][1];	//Obtengo exponente.
+
+				if(!incogs[incogAct]);
+				{
+					//Si no existía la creo.
+					incogs[incogAct]=expoAct;
+				}
+				else
+				{
+					
+				}
+				if(refPolIncogs[clave][j].incogs[k])
+			}
+		}
+		cohefs[i]=polArr[i][clave][1];			//Voy creando la lista de coheficientes.
+		refPolCohef[polArr[i][clave][2]]=polArr[i];	//Y asocio el coheficiente con el monomio correspondiente.
+	}
+	log.array();
+	log.array(cohefs);
+	log.array();
+	return 0;
+	//Ordeno la lista de coheficientes.
 	cohefs=cohefs.sort
 	(
 		function(a,b)
@@ -157,8 +524,10 @@ function reordenaCohef(polArr)
 			return a-b
 		}
 	);
-	
+	this.log.txt("Ordenada lista de coheficientes:");
+	this.log.txt("Ordenando polinomio según lista de coheficientes:");
 	log.array();
+	//Ordeno el polinomio segun la lista de coheficientes.
 	for(var j=0;j<polArr.length;j++)
 	{
 		ordenado[j]=refPolCohef[cohefs[j]];
@@ -166,7 +535,7 @@ function reordenaCohef(polArr)
 		log.array(ordenado[j]);
 	};
 	log.array();
-	return ordenado;
+	return ordenado;*/
 };
 //Suma los que tienen igual exponente.
 function combinaIgualExp(polOrdenado)
@@ -188,11 +557,11 @@ function combinaIgualExp(polOrdenado)
 			polCombinado.push(polOrdenado[j]);				//Voy coleccionando los polinomios en un array.
 			exponentes[expoAct]=polCombinado[polCombinado.length-1];	//Dejo en claro que el exponente está siendo usado.
 
-			log.txt("Guardado exponente en lista");
+			this.log.txt("Guardado exponente en lista");
 		}
 		else
 		{
-			log.txt
+			this.log.txt
 			("El exponente "+expoAct+" ya está en la lista , se suman los coheficientes "+exponentes[expoAct][0]+" + "+polOrdenado[j][0]+" = "+exponentes[expoAct][0]+polOrdenado[j][0]
 			);
 			exponentes[expoAct][0]+=polOrdenado[j][0];			//Como hay un polinomio usando este exponente, sumo los coheficientes.
@@ -254,40 +623,17 @@ function verifSignoCohef(polArr)
 	return cantidad;
 };
 
+interp=new Interp();
+log=interp.log;
+
 function procesaPol(txt)
 {
-	var pol,proc;
-	
+	interp.interpStr(txt);
+	//proc=reordenaCohef(proc);
 	log.sep();
-	log.fn("clrStrPol()");
-
-	proc=clrStrPol(txt);
-
-	log.txt(proc);
-	log.sep();
-	log.fn("strPolArr()");
-
-	proc=strPolArr(proc);
-	
-	log.array();
-	log.array(proc);
-	log.array();
-
-	log.sep();
-	log.txt("Convirtiendo monomios en arrays para coleccionarlos:");
-	pol=[];
-	for(var j=0;j<proc.length;j++)
-	{
-		pol[j]=strMonArr(proc[j]);
-	}
-	return 0;
-	log.sep();
-	log.fn("reordenaCohef()");
-
-	pol=reordenaCohef(pol);
 	//pol=combinaIgualExp(pol);
-	
-	return completaPolArr(pol);
+	return 0;
+	//return completaPolArr(proc);
 }
 function outinput()
 {
---
+++ log.js
@@ -0,0 +1,90 @@
+Log=function()
+{
+	this.buff="";
+	this.splTxt=":::::::::::::::::::::::::::::::::::::::::::::::::::";
+	this.enable=true;
+	this.arrayDef=false;
+}
+
+Log.prototype.isAble=function()
+{
+	if(!this.enable)
+	{
+		return;
+	}
+};
+Log.prototype.fn=function(nombre)
+{	
+	this.isAble();
+	this.buff+="<h2>"+nombre+"</h2>";
+};
+Log.prototype.sep=function()
+{
+	this.isAble();
+	this.buff+="<p>"+this.splTxt+"</p>";
+};
+Log.prototype.br=function(num)
+{
+	for(var j=0;j<num;j++)
+	{
+		this.buff+="<br>";
+	}
+}
+Log.prototype.txt=function()
+{
+	for(var j=0;j<arguments.length;j++)
+	{
+		this.buff+="<p>"+arguments[j]+"</p>";
+	}
+}
+Log.prototype.arrayStr=function(param,clave)
+{
+	this.isAble();
+
+	var tipo=param.constructor.toString();
+	var esArr=tipo.indexOf("Array()")!=-1;
+	var esObj=tipo.indexOf("Object()")!=-1;
+	var clase="arrEle";
+	var sep=["[","]"];
+	
+	if(esObj)
+	{
+		clase="objEle";
+		sep=["{","}"];
+	}
+	if(esArr || esObj)
+	{
+		this.buff+=" "+sep[0]+" ";
+		for(var clave in param)
+		{
+			this.buff+="<font class='"+clase+"'>"+clave+":</font>";
+			this.arrayStr(param[clave],clave);
+		}
+		this.buff=this.buff.substr(0,this.buff.length-1)+" "+sep[1]+" ";
+	}
+	else
+	{
+		this.buff+=" "+param+" ,";
+	}
+}
+Log.prototype.array=function()
+{
+	this.isAble();
+	if(this.arrayDef)
+	{
+		if(arguments.length)
+		{
+			this.arrayStr(arguments[0],arguments[1]);
+		}
+		else
+		{
+			this.buff+=")</p>"
+			this.arrayDef=false;
+		}
+	}
+	else
+	{
+		this.buff+="<p>Array(";
+		this.arrayDef=true;
+	}
+}
